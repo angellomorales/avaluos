1fb033c830c7017cea447b4d71994fe96abb870d
angellomorales <ing.angellomorales@gmail.com>
2020-12-21 22:49:10 -0500

    create logic app structure

--- predios/urls.py
+++ predios/urls.py
@@ -3,8 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
+app_name="predios"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:tipo_listado>", views.lista, name="listado"),
+    path("detallado/<int:predio_id>", views.detallado, name="detallado")
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- predios/views.py
+++ predios/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Predio, CategoryChoices
 
 
 def index(request):
 
     return render(request, "predios/index.html")
+
+
+def lista(request, tipo_listado):
+    if request.method == "GET":
+        tipoLista = request.GET["tipo"]
+        listado = Predio.objects.filter(category=tipo_listado)
+
+    context = {
+        "listado": listado
+    }
+    return render(request, "predios/listado.html", context)
+
+
+def detallado(request, predio_id):
+    try:
+        predioActual=Predio.objects.get(pk=predio_id)
+    except ObjectDoesNotExist:
+        predioActual=None
+    
+    context = {
+        "predio":predioActual
+    }
+    return render(request, "predios/detallado.html",context)
