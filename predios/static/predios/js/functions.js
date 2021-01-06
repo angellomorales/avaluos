d7ba6be5300b586b157c78f78c4828c9c5c0aa51
angellomorales <ing.angellomorales@gmail.com>
2021-01-05 23:36:37 -0500

    enhance index css and start with javascript

---
+++ predios/static/predios/js/functions.js
@@ -0,0 +1,12 @@
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     alert('funciona');
+//     document.querySelector('#index-rural').onmouseover = () => {
+//         console.log("hover");
+//         // document.getElementById("index-image").src = "https://images.pexels.com/photos/461960/pexels-photo-461960.jpeg";
+//     };
+// });
+
+function hello(){
+    alert('Hello, world!')
+}
