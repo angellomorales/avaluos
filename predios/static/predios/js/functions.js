a6366fa34dcc19dbb27b5b7c0cc2929c650ccc6a
angellomorales <ing.angellomorales@gmail.com>
2021-01-06 00:29:56 -0500

    enhance index css and start with javascript

--- predios/static/predios/js/functions.js
+++ predios/static/predios/js/functions.js
@@ -1,12 +1,24 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     alert('funciona');
-//     document.querySelector('#index-rural').onmouseover = () => {
-//         console.log("hover");
-//         // document.getElementById("index-image").src = "https://images.pexels.com/photos/461960/pexels-photo-461960.jpeg";
-//     };
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    // alert('funciona');
+    document.querySelector('#index-rural').onmouseover = () => {
+        // console.log("hover1");
+        cargarImagen(1);
+        
+    };
+    document.querySelector('#index-urbano').onmouseover = () => {
+        // console.log("hover2");
+        cargarImagen(2);
+        
+    };
+});
 
-function hello(){
-    alert('Hello, world!')
+function cargarImagen(imag){
+    if(imag==1){
+        document.getElementById("index-image").src = "https://images.pexels.com/photos/461960/pexels-photo-461960.jpeg";
+    }else{
+        document.getElementById("index-image").src = "https://images.pexels.com/photos/2679956/pexels-photo-2679956.jpeg";
+    }
+
 }
+
