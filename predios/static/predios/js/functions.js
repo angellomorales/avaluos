e8f525cc7bcc2655d8895b60565787770e032e34
angellomorales <ing.angellomorales@gmail.com>
2021-01-12 00:03:23 -0500

    add javascript hide-move animation to index image

--- predios/static/predios/js/functions.js
+++ predios/static/predios/js/functions.js
@@ -1,24 +1,31 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     // alert('funciona');
     document.querySelector('#index-rural').onmouseover = () => {
-        // console.log("hover1");
-        cargarImagen(1);
-        
+        const el = document.getElementById("index-image");
+        el.classList.add("animate-img")
+        cargarImagen(1,el);
+
     };
     document.querySelector('#index-urbano').onmouseover = () => {
+        const el = document.getElementById("index-image");
+        el.classList.add("animate-img")
         // console.log("hover2");
-        cargarImagen(2);
-        
+        cargarImagen(2,el);
+
     };
 });
 
-function cargarImagen(imag){
-    if(imag==1){
-        document.getElementById("index-image").src = "https://images.pexels.com/photos/461960/pexels-photo-461960.jpeg";
-    }else{
-        document.getElementById("index-image").src = "https://images.pexels.com/photos/2679956/pexels-photo-2679956.jpeg";
-    }
+function cargarImagen(imag,el) {
+    el.style.animationPlayState = 'running';
+    el.addEventListener('animationend', () => {
+        
+        if (imag == 1) {
+            el.src = "https://images.pexels.com/photos/461960/pexels-photo-461960.jpeg";
+        } else {
+            el.src = "https://images.pexels.com/photos/2679956/pexels-photo-2679956.jpeg";
+        }
+        el.classList.remove("animate-img")
+    });
+
 
 }
-
